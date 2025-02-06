02b20a5971112b789687c8f34e5676956ab21806
Karf2411 <abdullhsamir2@gmail.com>
2025-02-06 16:39:42 +0200

    first commit

---
+++ lib/Views/home_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [],
+    );
+  }
+}
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [],
+    );
+  }
+}
